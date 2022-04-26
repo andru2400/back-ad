ff416c629849bd45ce2c358b2522940eeb4c69fd
andru <andru2400@hotmail.com>
2022-04-26 14:40:39 -0500

    add api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('random', function () {
+    $numero = 5000;
+    // $numero = rand(5, 15);
+    return response()->json(['status' => 'ok', 'numero' => $numero , 'proyecto' => env('CURRENT_APPLICATION_KEY')], 200);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
